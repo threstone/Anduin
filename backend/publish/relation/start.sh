5db8b3fd0108f882836589488fdf1fc7e5bd350e
threstone <790325011@qq.com>
2023-04-24 17:59:25 +0800

    update launch script

--- backend/publish/relation/start.sh
+++ backend/publish/relation/start.sh
@@ -7,7 +7,7 @@ check() {
 start() {
         check
         if [ -z $pid ]; then
-           nohup node --title=${log_client} ./src/bin/main.js socketListenPort=9001 nodeId=Relation1 > logfile.log 2>&1 &
+           nohup node --title=${log_client} ./src/bin/main.js socketListenPort=9001 nodeId=Relation1> logfile.log 2>&1 &
                 sleep 3
                 check
                 echo -e "${log_client} pid is:\033[31m$pid\033[0m"
