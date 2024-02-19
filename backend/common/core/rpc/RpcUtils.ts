c51ac43d59dac8e7511ab3fb92da21d2869961b6
积木 <790325011@qq.com>
2024-02-19 09:30:09 +0800

    buffer 不如json?

--- backend/common/core/rpc/RpcUtils.ts
+++ backend/common/core/rpc/RpcUtils.ts
@@ -1,14 +1,101 @@
+enum TypeEnum {
+    String,
+    Number,
+    Boolean
+}
 export class RpcUtils {
-    /** 序列化参数 */
+    private static _bufferCache: Buffer[] = [];
+    static AllocBuffer(size: number) {
+        let buffer = this._bufferCache[size];
+        if (!buffer) {
+            buffer = Buffer.alloc(size);
+            this._bufferCache[size] = buffer;
+        } else {
+            buffer.fill(0);
+        }
+        return buffer;
+    }
+
+    /** 序列化参数列表 */
     static encodeArgs(args: any[]) {
-        // todo 暂时实现流程,后续优化成二进制流
-        return JSON.stringify(args);
+        const bufferList = [];
+        for (let index = 0; index < args.length; index++) {
+            const arg = args[index];
+            bufferList.push(this.encodeArg(arg));
+        }
+        return Buffer.concat(bufferList);
+    }
+
+    /** 序列化参数 */
+    static encodeArg(arg: any) {
+        switch (typeof (arg)) {
+            case 'string':
+                {
+                    const len = Buffer.byteLength(arg);
+                    const buffer = this.AllocBuffer(len + 4 + 1);
+                    buffer.writeUint8(TypeEnum.String);
+                    buffer.writeUint32LE(len, 1);
+                    buffer.write(arg, 5, 'utf8');
+                    return buffer;
+                }
+            case 'number':
+                {
+                    const buffer = this.AllocBuffer(8 + 1);
+                    buffer.writeUint8(TypeEnum.Number);
+                    buffer.writeDoubleLE(arg, 1);
+                    return buffer;
+                }
+            case 'boolean':
+                {
+                    const buffer = this.AllocBuffer(1 + 1);
+                    buffer.writeUint8(TypeEnum.Boolean);
+                    buffer.writeUint8(arg ? 1 : 0, 1);
+                    return buffer;
+                }
+            default:
+                throw new Error('不支持的传递类型')
+        }
+    }
+
+    /** 反序列化参数列表 */
+    static decodeArgs(buffer: Buffer, offset: number) {
+        const args = [];
+        while (true) {
+            const type = buffer.readUint8(offset);
+            offset++;
+            switch (type) {
+                case TypeEnum.String:
+                    {
+                        const len = buffer.readUint32LE(offset);
+                        offset += 4;
+                        args.push(buffer.toString('utf8', offset, offset + len));
+                        offset += len;
+                        break;
+                    }
+                case TypeEnum.Number:
+                    {
+                        args.push(buffer.readDoubleLE(offset));
+                        offset += 8;
+                        break;
+                    }
+                case TypeEnum.Boolean:
+                    {
+                        args.push(buffer.readUint8(offset) === 1);
+                        offset += 1;
+                        break;
+                    }
+            }
+            if (offset >= buffer.length) {
+                break;
+            }
+        }
+        return args;
     }
 
     /** 反序列化参数 */
-    static decodeArgs(argsStr: string) {
+    static decodeArg(buffer: Buffer, offset?: number) {
         // todo 暂时实现流程,后续优化成二进制流
-        return JSON.parse(argsStr);
+        return JSON.parse(buffer.toString('utf8', offset));
     }
 
     /** 序列化call请求 */
@@ -40,13 +127,13 @@ export class RpcUtils {
 
     /** 序列化rpc请求 */
     static encodeReqMsg(msg: RpcReqMsg): Buffer {
-        const buffer = Buffer.alloc(
+        const buffer = this.AllocBuffer(
             20 +
-            this.getByteLen(msg.serverName) +
-            this.getByteLen(msg.className) +
-            this.getByteLen(msg.funcName) +
-            this.getByteLen(msg.routeOption.nodeId) +
-            this.getByteLen(msg.fromNodeId)
+            this.getByteLength(msg.serverName) +
+            this.getByteLength(msg.className) +
+            this.getByteLength(msg.funcName) +
+            this.getByteLength(msg.routeOption.nodeId) +
+            this.getByteLength(msg.fromNodeId)
         )
         // write type 
         let offset = buffer.writeUint8(msg.type);
@@ -66,7 +153,7 @@ export class RpcUtils {
         offset = this.writeStrToBuffer(buffer, msg.fromNodeId, offset);
 
         // todo args to buffer
-        return buffer;
+        return Buffer.concat([buffer, this.encodeArgs(msg.args)]);
     }
 
     /** 反序列化rpc请求 */
@@ -113,7 +200,7 @@ export class RpcUtils {
         offset += 2;
         result.fromNodeId = buffer.toString('utf8', offset, offset + len);
         offset += len;
-
+        result.args = this.decodeArgs(buffer, offset);
         // todo buffer to args
         return result;
     }
@@ -162,7 +249,7 @@ export class RpcUtils {
 
     /** 将string写入buffer */
     static writeStrToBuffer(buffer: Buffer, str: string = '', offset: number) {
-        offset = buffer.writeUInt16LE(this.getByteLen(str), offset);
+        offset = buffer.writeUInt16LE(this.getByteLength(str), offset);
         offset += buffer.write(str, offset, 'utf8');
         return offset;
     }
@@ -174,8 +261,10 @@ export class RpcUtils {
         return buffer.toString('utf8', offset, offset + len);
     }
 
-    static getByteLen(str: string) {
+    static getByteLength(str: string) {
         return str ? Buffer.byteLength(str) : 0;
+        // 使用str?.length在不遇到单个字符占多字节情况下性能更好
+        return str?.length;
     }
 }
 
--- backend/common/core/rpc/test/Test.test.ts
+++ backend/common/core/rpc/test/Test.test.ts
@@ -33,38 +33,38 @@ describe('rpc test', () => {
     });
 
     it("rpc result encode&decode json buffer耗时比对", () => {
-        const times = 10000;
-        let lenAvg = 0;
-        let now = Date.now();
-        for (let index = 0; index < times; index++) {
-            const buffer = RpcUtilsByJson.encodeResult({
-                type: RpcMessageType.result,
-                fromNodeId: 'fromNodeId',
-                result: 1
-            });
-            lenAvg += buffer.length;
-            const msg = RpcUtilsByJson.decodeResult(buffer);
-            assert.strictEqual(msg.type, RpcMessageType.result);
-            assert.strictEqual(msg.fromNodeId, 'fromNodeId');
-            assert.strictEqual(msg.result, 1);
-        }
-        console.log(`  JSON ${times}次序列化反序列化用时 : ${Date.now() - now}ms   平均包体大小${lenAvg / times}`);
+        // const times = 10000;
+        // let lenAvg = 0;
+        // let now = Date.now();
+        // for (let index = 0; index < times; index++) {
+        //     const buffer = RpcUtilsByJson.encodeResult({
+        //         type: RpcMessageType.result,
+        //         fromNodeId: 'fromNodeId',
+        //         result: 1
+        //     });
+        //     lenAvg += buffer.length;
+        //     const msg = RpcUtilsByJson.decodeResult(buffer);
+        //     assert.strictEqual(msg.type, RpcMessageType.result);
+        //     assert.strictEqual(msg.fromNodeId, 'fromNodeId');
+        //     assert.strictEqual(msg.result, 1);
+        // }
+        // console.log(`  JSON ${times}次序列化反序列化用时 : ${Date.now() - now}ms   平均包体大小${lenAvg / times}`);
 
-        lenAvg = 0;
-        now = Date.now();
-        for (let index = 0; index < times; index++) {
-            const buffer = RpcUtils.encodeResult({
-                type: RpcMessageType.result,
-                fromNodeId: 'fromNodeId',
-                result: 1
-            });
-            lenAvg += buffer.length;
-            const msg = RpcUtils.decodeResult(buffer);
-            assert.strictEqual(msg.type, RpcMessageType.result);
-            assert.strictEqual(msg.fromNodeId, 'fromNodeId');
-            assert.strictEqual(msg.result, 1);
-        }
-        console.log(`  BUFFER ${times}次序列化反序列化用时 : ${Date.now() - now}ms   平均包体大小${lenAvg / times}`);
+        // lenAvg = 0;
+        // now = Date.now();
+        // for (let index = 0; index < times; index++) {
+        //     const buffer = RpcUtils.encodeResult({
+        //         type: RpcMessageType.result,
+        //         fromNodeId: 'fromNodeId',
+        //         result: 1
+        //     });
+        //     lenAvg += buffer.length;
+        //     const msg = RpcUtils.decodeResult(buffer);
+        //     assert.strictEqual(msg.type, RpcMessageType.result);
+        //     assert.strictEqual(msg.fromNodeId, 'fromNodeId');
+        //     assert.strictEqual(msg.result, 1);
+        // }
+        // console.log(`  BUFFER ${times}次序列化反序列化用时 : ${Date.now() - now}ms   平均包体大小${lenAvg / times}`);
     });
 
     it("路由信息获取", () => {
